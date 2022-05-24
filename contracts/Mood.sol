a6fbaa319ce1e7cee05ad28d6a4e82636796d932
Jude Eigbiremonlen <67139158+juddee@users.noreply.github.com>
2022-05-24 18:52:25 +0100

    Add files via upload

--- contracts/Mood.sol
+++ contracts/Mood.sol
@@ -1,1 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Mood
+ * @dev Store & retrieve value in a variable
+ */
+contract MoodDairy {
+
+    string mood;
+
+    /**
+     * @dev Store value in variable
+     * @param _mood value to mood
+     */
+
+    // this will set the mood
+    function setMood (string memory _mood) public {
+        mood = _mood;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'mood'
+     */
+    //  this will get the mood
+    function getMood() public view returns (string memory){
+        return mood;
+    }
+}
